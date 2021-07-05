b5499b10907a314114bf82c56fc3204ae8c2d912
Jahid101 <jahidhasananik.official@gmail.com>
2021-07-05 19:00:05 +0600

    day 2

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 
 const Create = () => {
@@ -6,6 +7,7 @@ const Create = () => {
     const [validStatus, setValidStatus] = useState(false);
     const [fieldName, setFieldName] = useState([]);
     const [userInfo, setUserInfo] = useState([]);
+    const [submitted, setSubmitted] = useState([]);
     const [allField, setAllField] = useState([]);
     const [fieldInfo, setFieldInfo] = useState([]);
 
@@ -31,52 +33,38 @@ const Create = () => {
     console.log(allField)
 
     useEffect(() => {
-        allField.map(user=>setFieldInfo(user)) 
+        allField.map(user => setFieldInfo(user))
     }, [allField])
     console.log(fieldInfo)
-
-    // useEffect(() => {
-    //     for (let i = 0; i < userInfo.length; i++) {
-    //         const element = userInfo.fieldName[i]
-    //         console.log(element)
-    //     }
-    // }, [userInfo,fieldName])
 
 
 
     const handleSubmit = (e) => {
-        const userInfo = {
-            id: e.target.id.value,
-            FullName: e.target.FullName.value
-        };
+        
+        e.preventDefault();
 
         if (validStatus) {
-            const url = `http://localhost/api/submit_form.php`;
-            fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(userInfo)
-            })
-                .then(res => res.json())
-                .then(data => {
-                    if (data) {
-                        alert('User added successfully')
-                        e.target.reset();
-                    }
-                })
+            fetch('http://localhost/api/submit_form.php')
+            .then(res => res.json())
+            .then(data => setSubmitted(data))
         }
         else {
             alert('Type only letters')
         }
-        e.preventDefault();
+        if(submitted.status === 'success') {
+            for (let i = 0; i < submitted.messages.length; i++) {
+                alert(submitted.messages[i])
+            }
+            e.target.reset();
+        }
     }
+    
+    console.log(submitted.messages)
 
 
     const handleBlur = (e) => {
         let isFieldValid = true;
-        if (e.target.name === "FullName") {
+        if (e.target.name === "user_name") {
             isFieldValid = /^[A-Za-z]+$/.test(e.target.value);
         }
         if (isFieldValid) {
@@ -85,27 +73,52 @@ const Create = () => {
         else {
             setValidStatus(false)
             alert("Type only letters")
+            e.target.value = '';
         }
     }
 
 
+    // const { register, handleSubmit } = useForm();
+    // const onSubmit = data => console.log(data);
+
+    var i = 0;
 
     return (
         <div className="container mt-5">
             <form onSubmit={handleSubmit}>
-                {fieldName.map(fieldName =>
+
+                {/* {fieldName.map(fieldName => */}
+                {allField.map(allField =>
                     <div className="form-group">
-                        <label>{fieldName}</label>
-                        <input type="text" className="form-control w-50" id="formGroupExampleInput" placeholder={fieldInfo.title} />
-                    </div>
-                )}
-                <br />
+                        <label>{allField.title}</label>
+
+                        {/* {fieldName.map(fieldName => */}
+                        <input
+                            onBlur={handleBlur}
+                            name={fieldName[i++]}
+                            type={allField.type}
+                            className={allField.html_attr.class}
+                            id={allField.html_attr.id}
+                            defaultValue={allField.html_attr.data_something}
+                            placeholder={allField.title}
+                            required={allField.required}
+                            validate={allField.validate}
+                        // {...register(fieldName[i++],{ pattern: /^[A-Za-z]+$/i })}
+                        />
+                        {/* )} */}
+
+                        <br />
+                        {/* )} */}
+                    </div>)}
+
                 {/* <div class="form-group">
                     <label for="formGroupExampleInput2">Another label</label>
                     <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input" />
                 </div> */}
 
+                <br />
                 <input className="btn btn-success mb-3" type="submit" value="Submit" />
+                
             </form>
         </div>
     );
